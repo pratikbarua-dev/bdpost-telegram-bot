4b1b70ba8583897d5d9a9ed64c9a82183db21dba
Pro-Tik <baruaprathik06@gmail.com>
2026-09-09 21:17:04 +0600

    Fix syntax and register delivered and received command handlers in bot.py

--- bot.py
+++ bot.py
@@ -8,9 +8,7 @@ import config
 from database.db import Database
 from handlers.start import start_handler, help_handler
 from handlers.tracking import track_command, status_command
-    from handlers.commands import my_command, stop_command, name_command, delivered_command
-    application.add_handler(CommandHandler("delivered", delivered_command))
-    application.add_handler(CommandHandler("received", delivered_command))
+from handlers.commands import my_command, stop_command, name_command, delivered_command
 from handlers.feedback import feedback_command
 from handlers.directory import postcode_command
 from handlers.admin import admin_command
@@ -86,6 +84,8 @@ def main() -> None:
     application.add_handler(CommandHandler("track", track_command))
     application.add_handler(CommandHandler("status", status_command))
     application.add_handler(CommandHandler("my", my_command))
+    application.add_handler(CommandHandler("delivered", delivered_command))
+    application.add_handler(CommandHandler("received", delivered_command))
     application.add_handler(CommandHandler("stop", stop_command))
     application.add_handler(CommandHandler("name", name_command))
     application.add_handler(CommandHandler("rename", name_command))
